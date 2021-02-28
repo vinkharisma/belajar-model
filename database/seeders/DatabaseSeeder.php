8551637e9596afda8d84b4e3ffff681cb3d1095b
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-03-01 02:54:15 +0700

    add seeder variation example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -17,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PostSeeder::class,
         ]);
+        Post::factory(100)->create();
     }
 }
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -14,6 +14,12 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        Post::factory(100)->create();
+        DB::table('posts')->insert([
+            'title' => 'Belajar Model Dengan Laravel',
+            'slug' => ' belajar-model-dengan-laravel',
+            'image' => 'https://via.placeholder.com/640x480.png/00ff77?text=Belajar Model',
+            'content' => 'Belajar dengan Laravel sungguh menyenangkan',
+            'draft' => 0
+        ]);
     }
 }
